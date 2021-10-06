b1599c825d4142094e4269300135017cce46f861
DESKTOP-OHD6DA9\USER <khjgogle@gmail.com>
2021-10-06 20:05:06 +0900

    Json Set

---
+++ Line Project/Assets/1_Scripts/Base/User.cs
@@ -0,0 +1,6 @@
+[System.Serializable]
+public class User
+{
+    public long gold;
+    public long best_Score;
+}
--- Line Project/Assets/1_Scripts/Manager/GameManager.cs
+++ Line Project/Assets/1_Scripts/Manager/GameManager.cs
@@ -1,12 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class GameManager : MonoSingleton<GameManager>
 {
     private GameState state; //씬 상태
     private List<Component> components = new List<Component>();
 
+
+    [SerializeField]
+    private User user;
+    private string SAVE_PATH = "";
+    private readonly string SAVE_FILENAME = "/SaveFile.txt";
+    private void Awake()
+    {
+        SAVE_PATH = Application.dataPath + "/Save"; //모바일때는 persistenDathPath로 변환
+        if(!Directory.Exists(SAVE_PATH))
+        {
+            Directory.CreateDirectory(SAVE_PATH);
+        }
+        LoadFromJson();
+
+        components.Add(new UiComponent());
+
+        UpdateState(GameState.INIT);
+
+        InvokeRepeating("SaveToJson", 0f, 60f);
+    }
+
+    private void LoadFromJson()
+    {
+        string json = "";
+
+        if(File.Exists(SAVE_PATH + SAVE_FILENAME))
+        {
+            json = File.ReadAllText(SAVE_PATH + SAVE_FILENAME);
+            user = JsonUtility.FromJson<User>(json);
+        }
+        else
+        {
+            SaveToJson();
+            LoadFromJson();
+        }
+    }
+    private void SaveToJson()
+    {
+        SAVE_PATH = Application.dataPath + "/Save";
+        if (user == null) return;
+        string json = JsonUtility.ToJson(user, true);
+        File.WriteAllText(SAVE_PATH + SAVE_FILENAME, json, System.Text.Encoding.UTF8);
+    }
+    private void OnApplicationPause(bool pause)
+    {
+        SaveToJson();
+    }
+    private void OnApplicationQuit()
+    {
+        SaveToJson();
+    }
     public void UpdateState(GameState state)
     {
         //상태 변경
@@ -19,11 +71,5 @@ public class GameManager : MonoSingleton<GameManager>
         if (state == GameState.INIT)
             UpdateState(GameState.STANDBY);
 
-    }
-    private void Awake()
-    {
-        components.Add(new UiComponent());
-
-        UpdateState(GameState.INIT);
     }
 }
