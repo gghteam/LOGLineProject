22269f1413fcf2df426d0dac55d223b0d28854aa
DESKTOP-OHD6DA9\USER <khjgogle@gmail.com>
2021-10-07 19:26:03 +0900

    Dotween Setting & 설정 toggle

---
+++ Line Project/Assets/1_Scripts/Manager/UiManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class UiManager : MonoBehaviour
+{
+    //bool isSetting = false;
+    [SerializeField]
+    private RectTransform settingPanel;
+
+    public void OpenUI(GameObject ui)
+    {
+        ui.SetActive(true);
+    }
+    public void CloseUI(GameObject ui)
+    {
+        ui.SetActive(false);
+    }
+    /*
+    public void SettingMode(GameObject setting)
+    {
+        if (isSetting)
+            setting.SetActive(false);
+        else
+            setting.SetActive(true);
+        isSetting = isSetting ? isSetting = false : isSetting = true;
+    }
+    */
+
+    public void OnToggleChanged(bool isOn)
+    {
+        settingPanel.DOAnchorPosX(isOn ? 0f : 1400f, 0.2f).SetEase(Ease.InCirc);
+        Debug.Log("ch");
+    }
+}
