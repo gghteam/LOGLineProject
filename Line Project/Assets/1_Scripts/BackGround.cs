2f12d596da0ad2dbd78db97f55c1b20aaff89bb5
DESKTOP-OHD6DA9\USER <jetmax68@gmail.com>
2021-10-20 19:53:45 +0900

    no message

--- Line Project/Assets/1_Scripts/BackGround.cs
+++ Line Project/Assets/1_Scripts/BackGround.cs
@@ -4,22 +4,4 @@ using UnityEngine;
 
 public class BackGround : MonoBehaviour
 {
-    private MeshRenderer render;
-
-    [SerializeField]
-    private float speed;
-
-    private float offset;
-    // Start is called before the first frame update
-    void Start()
-    {
-        render = GetComponent<MeshRenderer>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        offset += Time.deltaTime * speed;
-        render.material.mainTextureOffset = new Vector2(0, offset);
-    }
 }
