74296a03d0c4fc3f95953d3f0802d72c21a830bf
DESKTOP-OHD6DA9\USER <jetmax68@gmail.com>
2021-11-03 16:42:41 +0900

    버그수정

--- Line Project/Assets/1_Scripts/Line/LineComponent.cs
+++ Line Project/Assets/1_Scripts/Line/LineComponent.cs
@@ -28,37 +28,43 @@ public class LineComponent : MonoBehaviour
         for (int i = 0; i < 20;)
         {
             int j = 0;
-            /*int RandomNumber = Random.Range(0, 101);
-            GameObject C = RandomNumber >= 80 ? PoolManager.Instance.GetPooledObject(3) : PoolManager.Instance.GetPooledObject(0);
-            C.transform.SetParent(lineobject.transform);
-            C.transform.localPosition = d;
-            C.SetActive(true);
-            i++;
-            Debug.Log(i);*/
             for (j = 0; j < 3;)
             {
-                int RandomNumber2 = Random.Range(0, 101);
-                GameObject b = RandomNumber2 >= 80 ? PoolManager.Instance.GetPooledObject(3) : PoolManager.Instance.GetPooledObject(0);
-                b.transform.SetParent(lineobject.transform);
-                b.transform.localPosition = d;
-                b.SetActive(true);
-                i++;
-                j++;
-                Debug.Log(i);
-                d.x += 1.5f;
-            };
+                if (i<6)
+                {
+                    GameObject c = PoolManager.Instance.GetPooledObject(0);
+                    c.transform.SetParent(lineobject.transform);
+                    c.transform.localPosition = d;
+                    c.SetActive(true);
+                    d.x += 1.5f;
+                    i++;
+                    j++;
+                }
+                else
+                {
+                    int RandomNumber2 = Random.Range(0, 101);
+                    GameObject b = RandomNumber2 >= 80 ? PoolManager.Instance.GetPooledObject(3) : PoolManager.Instance.GetPooledObject(0);
+                    b.transform.SetParent(lineobject.transform);
+                    b.transform.localPosition = d;
+                    b.SetActive(true);
+                    i++;
+                    j++;
+                    Debug.Log(i);
+                    d.x += 1.5f;
+                }
+            }
             d.x = -1.5f;
             d.y += 10f;
         }
     }
     public void lineck()
     {
-        for(int i = 0; i<3; i++)
+        for (int i = 0; i < 3; i++)
         {
             Debug.Log(i);
             PoolManager.Instance.Despawn(lineobject.transform.GetChild(i).gameObject);
         }
-        for(int i = 0; i<3; i++)
+        for (int i = 0; i < 3; i++)
         {
             int RandomNumber = Random.Range(0, 101);
             GameObject C = RandomNumber >= 80 ? PoolManager.Instance.GetPooledObject(3) : PoolManager.Instance.GetPooledObject(0);
